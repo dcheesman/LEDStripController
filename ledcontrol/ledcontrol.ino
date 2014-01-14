74b65027ccc0851d2cc95677d864853067ba5c24
Sam Beveridge <sam.bev87@gmail.com>
2014-01-13 19:46:20 -0700

    Finally got the arduino side of things up to par.  We can now send 24+ frames to the board and it can keep up.

--- ledcontrol/ledcontrol.ino
+++ ledcontrol/ledcontrol.ino
@@ -1,51 +1,35 @@
-/*
-LED map test
-Read Serial IN and map values to led positions and color
- */
-#include "FastSPI_LED.h"
-
-const int NUM_LEDS = 10;
+#include <OctoWS2811.h>
 
 int id = 0;
 int rval = 0;
 int gval = 0;
 int bval = 0;
 
-// Sometimes chipsets wire in a backwards sort of way
-struct CRGB { unsigned char g; unsigned char r; unsigned char b; };
-// struct CRGB { unsigned char r; unsigned char g; unsigned char b; };
-struct CRGB *leds;
+const int ledsPerStrip = 10;
 
-const int DILED = 4;
+DMAMEM int displayMemory[ledsPerStrip*6];
+int drawingMemory[ledsPerStrip*6];
 
-void setup()
-{
-    FastSPI_LED.setLeds(NUM_LEDS);
-    FastSPI_LED.setChipset(CFastSPI_LED::SPI_WS2811);
+const int config = WS2811_GRB | WS2811_800kHz;
 
-    FastSPI_LED.setPin(DILED);
+OctoWS2811 leds(ledsPerStrip, displayMemory, drawingMemory, config);
 
-    FastSPI_LED.init();
-    FastSPI_LED.start();
-
-    leds = (struct CRGB*)FastSPI_LED.getRGBData(); 
-    Serial.begin(9600);
+void setup() {
+  leds.begin();
+  leds.show();
+  Serial.begin(9600);
 }
 
-void loop() 
-{
-    while (Serial.available() > 0) {
+void loop() {
+  while (Serial.available() > 0) {
         id = Serial.parseInt();
         rval = Serial.parseInt();
         gval = Serial.parseInt();
         bval = Serial.parseInt();
 
-        if (Serial.peek() == '\n') {
-            leds[id].r = rval;
-            leds[id].g = gval;
-            leds[id].b = bval;
-            FastSPI_LED.show();
+        if (Serial.read() == '\n') {
+            leds.setPixel(id, rval, gval, bval);
+            leds.show();
         }
     }
-    
 }
